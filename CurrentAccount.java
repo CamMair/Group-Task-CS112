3d0d89d436d18a02940cd052ac28a1820be5586d
Cameron Mair (Student) <cameron.mair.2023@uni.strath.ac.uk>
2024-03-18 14:29:25 +0000

    CA-2 Work In progress

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,5 +1,19 @@
 public class CurrentAccount{
     private Integer overdrawAmount;
-    public CurrentAccount() {
+    private String accountName;
+    private String accountID;
+
+    public CurrentAccount(String accountName, String accountID, Integer overdrawAmount) {
+        this.overdrawAmount = overdrawAmount;
+        this.accountName = accountName;
+        this.accountID = accountID;
+    }
+
+    public Integer getOverdrawAmount() {
+        return overdrawAmount;
+    }
+
+    public void setOverdrawAmount(Integer overdrawAmount) {
+        this.overdrawAmount = overdrawAmount;
     }
 }
