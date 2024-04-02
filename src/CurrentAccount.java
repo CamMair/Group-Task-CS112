d5c72612abcf3bfb2a3deced49d93c441f333eaf
Cameron Mair (Student) <cameron.mair.2023@uni.strath.ac.uk>
2024-04-02 22:24:10 +0100

    CA-2 finished

---
+++ src/CurrentAccount.java
@@ -0,0 +1,61 @@
+package src;
+
+
+public class CurrentAccount extends BankAccount{
+    private Integer overdrawAmount;
+    BankAccount bankAccount;
+
+    public CurrentAccount(String accountName, String accountID, Integer overdrawAmount) {
+        super(accountName, accountID);
+        setOverdrawAmount(overdrawAmount);
+    }
+
+
+
+    public void printDetails(CurrentAccount account){
+        System.out.println("Account Name: " + bankAccount.getAccountName());
+        System.out.println("Account id: " + bankAccount.getAccountNumber());
+        System.out.println("Balance: " + bankAccount.getBalance() );
+        System.out.println("Account Type: Current");
+        System.out.println("Overdraw: " + getOverdrawAmount());
+
+    }
+    public Integer getOverdrawAmount() {
+
+        return overdrawAmount;
+    }
+
+    public void setOverdrawAmount(Integer overdrawAmount) {
+        if (overdrawAmount < 0 ){
+            this.overdrawAmount = 0;
+        } else if (overdrawAmount > 300) {
+            this.overdrawAmount = 300;
+        } else {
+            this.overdrawAmount = overdrawAmount;
+        }
+    }
+
+    @Override
+    public boolean withdraw(float amount) {
+        float availableFunds = this.getBalance() + this.overdrawAmount;
+        float balanceBefore = this.getBalance();
+        if (balanceBefore > (this.getBalance() - amount)) {
+            if (availableFunds >= amount) {
+
+                this.deductBalance(amount);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Account Name: " + bankAccount.getAccountName());
+        System.out.println("Account id: " + bankAccount.getAccountNumber());
+        System.out.println("Balance: " + bankAccount.getBalance() );
+        System.out.println("Account Type: Current");
+        System.out.println("Overdraw: " + getOverdrawAmount());
+
+    }
+}
