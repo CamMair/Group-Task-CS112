5f99dad6394c98438187f8cd7719a9f10b540854
Fraser Nixon (Student) <fraser.nixon.2023@uni.strath.ac.uk>
2024-04-03 22:48:35 +0100

    deleted main

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Bank bank = new Bank();
-        CurrentAccount ca = new CurrentAccount("Cameron Mair", "CA1", 500);
-        bank.addAccount("ca", ca);
-        bank.printAllAccounts();
-    }
-}
